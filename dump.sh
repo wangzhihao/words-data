41b3458857a3000477404f1767b88f09d2dc7dfb
wang zhihao <accept.acm@gmail.com>
2015-06-06 00:48:23 -0400

    add name only version

--- dump.sh
+++ dump.sh
@@ -3,3 +3,5 @@
 ROOT_DIR=$( cd "$( dirname "$0" )" && pwd )
 
 mongoexport --db words --collection word --out $ROOT_DIR/word.json
+
+mongoexport --db words --collection word --fields 'name' --type=csv  --out $ROOT_DIR/name-only.csv
